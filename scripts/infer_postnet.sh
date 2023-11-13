e83a0d4ef0ade17c1e0c742e5dede04db2a0b978
gt <jack139@gmail.com>
2023-11-13 08:49:18 +0800

    fix script

--- scripts/infer_postnet.sh
+++ scripts/infer_postnet.sh
@@ -3,7 +3,7 @@ export Video_ID=May
 export Wav_ID=zozo
 export Postnet_Ckpt_Steps=4000 # please reach to `docs/train_models.md` to get some tips about how to select an approprate ckpt_steps!
 
-python inference/postnet/postnet_infer.py \
+LD_LIBRARY_PATH= python3.9 inference/postnet/postnet_infer.py \
     --config=checkpoints/${Video_ID}/lm3d_postnet_sync/config.yaml \
     --hparams=infer_audio_source_name=data/raw/val_wavs/${Wav_ID}.wav,\
 infer_out_npy_name=infer_out/${Video_ID}/pred_lm3d/${Wav_ID}.npy,\
